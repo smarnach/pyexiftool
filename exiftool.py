89e71e83ffbd0c0f310ed02edeac51284a2fd4d8
Sven Marnach <sven@marnach.net>
2012-04-11 18:32:29 +0100

    Add licence (GPL 3)

--- exiftool.py
+++ exiftool.py
@@ -1,5 +1,20 @@
-# PyExifTool (http://github.com/smarnach/pyexiftool)
+# PyExifTool <http://github.com/smarnach/pyexiftool>
 # Copyright 2012 Sven Marnach
+
+# This file is part of PyExifTool.
+#
+# PyExifTool is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PyExifTool is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PyExifTool.  If not, see <http://www.gnu.org/licenses/>.
 
 """PyExifTool
 
