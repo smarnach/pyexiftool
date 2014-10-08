3db3764895e687d75b42d3ae4e554ca8664a7f6f
Sven Marnach <sven@marnach.net>
2014-10-08 15:06:36 +0100

    Dual-licence the code under GPL and Revised BSD Licence.

--- setup.py
+++ setup.py
@@ -5,16 +5,14 @@
 #
 # PyExifTool is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# the Free Software Foundation, either version 3 of the licence, or
+# (at your option) any later version, or the BSD licence.
 #
 # PyExifTool is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 #
-# You should have received a copy of the GNU General Public License
-# along with PyExifTool.  If not, see <http://www.gnu.org/licenses/>.
+# See COPYING.GPL or COPYING.BSD for more details.
 
 from distutils.core import setup
 
