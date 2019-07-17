6a9124e40819a198c2ed2c1860332a9cdf412cfd
SylikC (admin) <sylikc@sylik.chi>
2019-07-17 00:51:29 -0700

    convert all leading spaces to tabs in Python code

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,8 @@
 # PyExifTool Changelog
 
+Current dates are in PST/PDT
 
-Date                   | Version | Comment
----------------------- | ------- | -------
-07/17/2019 12:26:16 AM | 0.1     | Source was pulled directly from https://github.com/smarnach/pyexiftool with a complete bare clone to preserve all history.  Because it's no longer being updated, I will pull all merge requests in and make updates accordingly
-
+Date (Timezone)              | Version | Comment
+---------------------------- | ------- | -------
+07/17/2019 12:26:16 AM (PDT) | 0.1     | Source was pulled directly from https://github.com/smarnach/pyexiftool with a complete bare clone to preserve all history.  Because it's no longer being updated, I will pull all merge requests in and make updates accordingly
+07/17/2019 12:50:20 AM (PDT) | 0.1     | Convert leading spaces to tabs
--- doc/conf.py
+++ doc/conf.py
@@ -183,8 +183,8 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'PyExifTool.tex', u'PyExifTool Documentation',
-   u'Sven Marnach', 'manual'),
+	('index', 'PyExifTool.tex', u'PyExifTool Documentation',
+	 u'Sven Marnach', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -213,8 +213,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'pyexiftool', u'PyExifTool Documentation',
-     [u'Sven Marnach'], 1)
+	('index', 'pyexiftool', u'PyExifTool Documentation',
+	 [u'Sven Marnach'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -227,9 +227,9 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'PyExifTool', u'PyExifTool Documentation',
-   u'Sven Marnach', 'PyExifTool', 'One line description of project.',
-   'Miscellaneous'),
+	('index', 'PyExifTool', u'PyExifTool Documentation',
+	 u'Sven Marnach', 'PyExifTool', 'One line description of project.',
+	 'Miscellaneous'),
 ]
 
 # Documents to append as an appendix to all manuals.
