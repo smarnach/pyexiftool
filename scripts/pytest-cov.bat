8281a5eee9e5fcb5a7d8d194fe86804502c94fa5
SylikC <sylikc@gmail.com>
2021-04-19 04:16:23 -0700

    fix pytest-cov script to only show coverage for exiftool, and not other included libraries from virtualenv

--- scripts/pytest-cov.bat
+++ scripts/pytest-cov.bat
@@ -8,9 +8,11 @@ echo PyTest Coverage Script
 echo;
 echo pip's PyTest version
 python -m pip show pytest | findstr /l /c:"Version:"
+echo pip's PyTest-cov version
+python -m pip show pytest-cov | findstr /l /c:"Version:"
 echo ______________________
 
-
-python.exe -m pytest -v --cov --cov-report term-missing
+REM added the --cov= so that it doesn't try to test coverage on the virtualenv directory
+python.exe -m pytest -v --cov=exiftool --cov-report term-missing tests/
 
 popd
